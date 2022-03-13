8b5ce16526d51a204a38bc34587acc8bd72f0ecb
Vitalya Dev <vitalya.dev@gmail.com>
2022-03-13 23:07:01 +0300

    Complete.

--- Scenes/Shared/BaseLevel.gd
+++ Scenes/Shared/BaseLevel.gd
@@ -1,7 +1,7 @@
 extends Control
 
 # Declare member variables here. Examples
-export(int) var scores_in_sec = 5
+export(int) var scores_in_sec = 7
 export(int) var mines = 5
 export (int) var bg_frame = 0
 
